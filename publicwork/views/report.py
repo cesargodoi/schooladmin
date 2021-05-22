1ba03d7cdddeb44371ecc81dc1e3cf70986e72c5
cesargodoi <cesar.godoi@gmail.com>
2021-05-22 16:08:25 -0300

    clear session on exit reports page

--- publicwork/views/report.py
+++ publicwork/views/report.py
@@ -13,6 +13,7 @@ from schooladmin.common import SEEKER_STATUS
 @login_required
 @permission_required("publicwork.view_lecture")
 def publicwork_home(request):
+    del request.session["search"]
     context = {"title": "Public work"}
     return render(request, "publicwork/publicwork_home.html", context)
 
